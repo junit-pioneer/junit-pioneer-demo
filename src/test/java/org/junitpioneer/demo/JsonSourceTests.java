92a22e804d2e6f6d701aa79f1c5b3801fd864262
Daniel Kraus <daniel.kraus@mailbox.org>
2023-10-05 22:44:57 +0200

    Format and optimize imports

--- src/test/java/org/junitpioneer/demo/JsonSourceTests.java
+++ src/test/java/org/junitpioneer/demo/JsonSourceTests.java
@@ -3,16 +3,14 @@ package org.junitpioneer.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junitpioneer.jupiter.json.JsonSource;
 import org.junitpioneer.jupiter.json.Property;
 
-
 class JsonSourceTests {
 
 	@ParameterizedTest
-	@JsonSource("[ { name: 'Luke', height: 172  }, { name: 'Yoda', height: 66 } ]")
+	@JsonSource("[ { name: 'Luke', height: 172 }, { name: 'Yoda', height: 66 } ]")
 	void jsonTest(@Property("name") String name, @Property("height") int height) {
 		if (name.equals("Luke"))
 			assertEquals(172, height);
